1a80bf1dee6412363c1bf38dc68f2495913bccee
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2025-10-27 12:28:45 +0200

    Add task 1 of js-recap 2

---
+++ tasks/week1/js-recap2/t1/script.js
@@ -0,0 +1,31 @@
+// Week 1 JS recap 2 - task 1
+
+// i
+const fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+
+// ii
+console.table(fruits);
+
+// iii
+const fruitsLength = fruits.length;
+console.log(fruitsLength);
+
+// iv
+console.log(fruits[2]);
+
+// v
+console.log(fruits[fruits.length - 1]);
+
+// vi
+const vegetables = [];
+
+// vii
+while (vegetables.length < 3) {
+  vegetables.push(prompt("Enter a vegetable:"));
+}
+
+// viii
+console.table(vegetables);
+
+// xi
+console.log(vegetables.length);
